495f67d9b33528ca4fbc1f0fb1555398da55fcd1
robin <robin@baldwin.be>
2022-04-08 18:44:59 +0200

    Added a console logger

---
+++ web/modules/custom/amqp/src/ConsoleLog.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Drupal\amqp;
+
+use Drupal\Core\Datetime\DrupalDateTime;
+use Drupal\Core\Logger\RfcLoggerTrait;
+use Drupal\Core\Logger\RfcLogLevel;
+use Psr\Log\LoggerInterface;
+use Robo\Common\IO;
+use Robo\Contract\IOAwareInterface;
+use Symfony\Component\Console\Output\ConsoleOutput;
+
+class ConsoleLog implements LoggerInterface, IOAwareInterface
+{
+
+  public const SUCCESS = 8;
+
+  use IO;
+  use RfcLoggerTrait;
+
+  public function __construct()
+  {
+    $this->setOutput(new ConsoleOutput());
+  }
+
+  public function log($level, $message, array $context = [])
+  {
+    $format = match ($level) {
+      RfcLogLevel::EMERGENCY => ' [%s] <fg=white;bg=red;options=bold>[emergency]</fg=white;bg=red;options=bold> %s',
+      RfcLogLevel::ALERT => ' [%s] <fg=white;bg=red;options=bold>[alert]</fg=white;bg=red;options=bold>     %s',
+      RfcLogLevel::ERROR => ' [%s] <fg=white;bg=red;options=bold>[error]</fg=white;bg=red;options=bold>     %s',
+      RfcLogLevel::CRITICAL => ' [%s] <fg=white;bg=red;options=bold>[critical]</fg=white;bg=red;options=bold>  %s',
+      RfcLogLevel::WARNING => ' [%s] <fg=black;bg=yellow;options=bold>[warning]</fg=black;bg=yellow;options=bold>   %s',
+      RfcLogLevel::NOTICE => ' [%s] <fg=black;bg=yellow;options=bold>[notice]</fg=black;bg=yellow;options=bold>    %s',
+      RfcLogLevel::INFO => ' [%s] <fg=default;bg=blue;options=bold>[info]</fg=default;bg=blue;options=bold>      %s',
+      RfcLogLevel::DEBUG => ' [%s] <fg=default;bg=blue;options=bold>[debug]</fg=default;bg=blue;options=bold>     %s',
+      self::SUCCESS => ' [%s] <fg=white;bg=green;options=bold>[success]</fg=white;bg=green;options=bold>   %s',
+    };
+
+    $this->writeln(sprintf($format, (new DrupalDateTime('now'))->format('H:i:s'), $message));
+  }
+}
--- web/modules/custom/amqp/src/Consumer.php
+++ web/modules/custom/amqp/src/Consumer.php
@@ -14,6 +14,7 @@ class Consumer
   public function __construct(
     private AMQPStreamConnection $connection,
     private AMQPChannelFactory $AMQPChannelFactory,
+    private ConsoleLog $consoleLog,
   )
   {
 
@@ -27,30 +28,32 @@ class Consumer
   public function consume(Queue $queue)
   {
     $channel = $this->AMQPChannelFactory->getForQueue($queue);
-    $this->log('Waiting for messages. To exit press CTRL+C');
+    $this->consoleLog->debug('Waiting for messages. To exit press CTRL+C');
 
     $worker = $queue->getWorker();
-    $this->log(sprintf(
+    $this->consoleLog->debug(sprintf(
       'Worker ready to receive up to %s messages until %s.',
       $worker->getMaxIterations(),
       $worker->getMaxLifeTime()->format('Y-m-d H:i:s'),
     ));
 
     $callback = static function (AMQPMessage $message) use ($worker) {
+      $consoleLog = new ConsoleLog();
       try {
         if ($worker->maxLifeTimeReached() || $worker->maxIterationsReached()) {
           throw new WorkerMaxLifeTimeOrIterationsExceeded();
         }
 
-        $this->log(sprintf('Worker %s processing message %s', $worker::class, $message->getDeliveryTag()));
+        $consoleLog->log(ConsoleLog::SUCCESS, sprintf('Worker %s processing message %s', $worker::class, $message->getDeliveryTag()));
         $worker->processMessage($message);
         $message->getChannel()->basic_ack($message->getDeliveryTag());
       } catch (WorkerMaxLifeTimeOrIterationsExceeded $e) {
         // Requeue message to make sure next consumer can process it.
+        $this->consoleLog->warning('Worker max life time or iterations exceeded. Re-queueing message for next consumer.');
         $message->getChannel()->basic_nack($message->getDeliveryTag(), false, true);
         throw $e;
       } catch (\Exception|\Error $exception) {
-        $this->error(sprintf('Worker %s could not process message: %s', $worker::class, $message->getDeliveryTag()));
+        $consoleLog->error(sprintf('Worker %s could not process message: %s', $worker::class, $message->getDeliveryTag()));
         $worker->processFailure($message, $exception);
       }
     };
@@ -62,19 +65,9 @@ class Consumer
         $channel->wait();
       }
     } catch (WorkerMaxLifeTimeOrIterationsExceeded) {
-      $this->log('Worker max life time or iterations exceeded. Closing connection.');
+      $this->consoleLog->warning('Worker max life time or iterations exceeded. Closing connection.');
       $channel->close();
       $this->connection->close();
     }
-  }
-
-  public function log(string $message): void
-  {
-    echo ' [*] ' . $message . PHP_EOL;
-  }
-
-  public function error(string $message): void
-  {
-    echo ' [X] ERROR: ' . $message . PHP_EOL;
   }
 }
